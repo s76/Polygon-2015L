80c1962263065e2b0b0250fa05cbf0e3805c4a7e
Macielos <maciek.ganko@gmail.com>
2015-04-01 02:10:23 +0200

    Camera and movement
    - Poruszamy się strzałkami
    - Za pomocą + i - możemy zoomować
    - Kamera śledzi naszego gracza
    
    - RaycastingTest - tylko zaczęte, nigdzie nie podpięte, trochę się
    bawiłem przechwytywaniem kliknięć myszy. Można wykorzystać przy
    strzelaniu.

---
+++ Root/Assets/CameraAndMovement/PlayerMovement.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	
+	public KeyCode moveUp; 
+	public KeyCode moveDown; 
+	public KeyCode moveLeft; 
+	public KeyCode moveRight; 
+	public KeyCode zoomIn; 
+	public KeyCode zoomOut; 
+
+	public const float PLAYER_SPEED = 5.0f;
+
+	public const float X_OFFSET = 0.0f;
+	public const float Y_OFFSET = 15.0f;
+	public const float Z_OFFSET = -10.0f;
+
+	public const float MIN_ZOOM = 0.5f;
+	public const float MAX_ZOOM = 2.0f;
+	public const float ZOOM_SPEED = 0.01f;
+	
+	static Vector3 UP = new Vector3 (0, 0, 0);
+	static Vector3 UP_RIGHT = new Vector3 (0, 45, 0);
+	static Vector3 RIGHT = new Vector3 (0, 90, 0);
+	static Vector3 DOWN_RIGHT = new Vector3 (0, 135, 0);
+	static Vector3 DOWN = new Vector3 (0, 180, 0);
+	static Vector3 DOWN_LEFT = new Vector3 (0, 225, 0);
+	static Vector3 LEFT = new Vector3 (0, 270, 0);
+	static Vector3 UP_LEFT = new Vector3 (0, 315, 0);
+
+	float zoom = 1.0f;
+		
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Rigidbody rigidbody = GetComponent<Rigidbody> (); 
+		Vector3 velocity = rigidbody.velocity;
+		Quaternion rotation = rigidbody.rotation;
+		Vector3 position = rigidbody.position;
+		Vector3 cameraPosition = Camera.main.transform.position;
+
+		if (Input.GetKey (zoomOut)) {
+			if(zoom < MAX_ZOOM){
+				zoom += ZOOM_SPEED;
+			}
+		} else if (Input.GetKey (zoomIn)) {
+			if(zoom > MIN_ZOOM){
+				zoom -= ZOOM_SPEED;
+			}
+		}
+
+		//make camera track player
+		cameraPosition.x = position.x+X_OFFSET*zoom;
+		cameraPosition.y = position.y+Y_OFFSET*zoom;
+		cameraPosition.z = position.z+Z_OFFSET*zoom;
+
+		//set player's z velocity
+		if (Input.GetKey (moveUp)) {
+			velocity.z = PLAYER_SPEED;
+		} else if (Input.GetKey (moveDown)) {
+			velocity.z = PLAYER_SPEED * -1;
+		} else {
+			velocity.z = 0;
+		}
+
+		//set player's x velocity
+		if (Input.GetKey (moveRight)) {
+			velocity.x = PLAYER_SPEED;
+		} else if (Input.GetKey (moveLeft)) {
+			velocity.x = PLAYER_SPEED * -1;
+		} else {
+			velocity.x = 0;
+		}
+
+		//set player's orientation
+		if (velocity.x == 0 && velocity.z > 0) {
+			rotation.eulerAngles = UP;
+		} else if (velocity.x > 0 && velocity.z > 0) {
+			rotation.eulerAngles = UP_RIGHT;
+		} else if (velocity.x > 0 && velocity.z == 0) {
+			rotation.eulerAngles = RIGHT;
+		} else if (velocity.x > 0 && velocity.z < 0) {
+			rotation.eulerAngles = DOWN_RIGHT;
+		} else if (velocity.x == 0 && velocity.z < 0) {
+			rotation.eulerAngles = DOWN;
+		} else if (velocity.x < 0 && velocity.z < 0) {
+			rotation.eulerAngles = DOWN_LEFT;
+		} else if (velocity.x < 0 && velocity.z == 0) {
+			rotation.eulerAngles = LEFT;
+		} else if (velocity.x < 0 && velocity.z > 0) {
+			rotation.eulerAngles = UP_LEFT;
+		}
+
+		Camera.main.transform.position = cameraPosition;
+		rigidbody.rotation = rotation;
+		rigidbody.velocity = velocity;
+	}
+}
